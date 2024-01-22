7c80709da1641b2a6831b074f6bc77e816d6561f
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-22 20:43:36 -0300

    feat: fix code

--- Exercise03/train-booking.ts
+++ Exercise03/train-booking.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
-const { stdin, stout, exit} = process 
+const { stdin, stdout, exit } = process;
 stdin.resume();
 stdin.setEncoding('utf-8');
 
@@ -8,23 +8,23 @@ let rawInput = "";
 stdin.on('data', function (data) {
     rawInput += data.toString().trim();
     rawInput += '\n';
-})
+});
 
 function solve(N: number, bookings: [number, number, number][]): number[] {
     const finalResult = new Array(N).fill(10000);
-    const result: { [key: number]: number} ={};
+    const result: { [key: number]: number } = {};
 
-    for (let i = 1; i<= N; i++) {
+    for (let i = 1; i <= N; i++) {
         result[i] = 0;
     }
 
     for (let booking of bookings) {
         for (let i = booking[0]; i <= booking[1]; i++) {
-            result[1] = result[1] + booking[2];
+            result[i] = result[i] + booking[2];
         }
     }
 
-    return finalResult.map((value, index) => value - result[index + 1])
+    return finalResult.map((value, index) => value - result[index + 1]);
 }
 
 stdin.on('end', function () {
@@ -37,10 +37,12 @@ stdin.on('end', function () {
     idx++;
     const bookings = input.slice(idx, idx + K).map(bookingData =>
         bookingData.trim().split(' ').map(el => parseInt(el, 10))
-        ) as [number, number, number][];
+    ) as [number, number, number][];
 
-        const output = solve(N, bookings);
-        stout.write(output.join(' ') + '\n');
-})
+    const output = solve(N, bookings);
+    stdout.write(output.join(' ') + '\n');
+
+    exit();
+});
 
 stdin.resume();
