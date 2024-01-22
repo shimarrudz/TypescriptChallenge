21b7ac1c55c71e053e6225351e41dd2997fbc4df
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-22 20:42:28 -0300

    feat: add output

--- Exercise03/train-booking.ts
+++ Exercise03/train-booking.ts
@@ -26,3 +26,21 @@ function solve(N: number, bookings: [number, number, number][]): number[] {
 
     return finalResult.map((value, index) => value - result[index + 1])
 }
+
+stdin.on('end', function () {
+    const input = rawInput.replace(/\n$/, "").split("\n");
+
+    let idx = 0;
+    const N = parseInt(input[idx++].trim(), 10);
+    const K = parseInt(input[idx++].trim(), 10);
+
+    idx++;
+    const bookings = input.slice(idx, idx + K).map(bookingData =>
+        bookingData.trim().split(' ').map(el => parseInt(el, 10))
+        ) as [number, number, number][];
+
+        const output = solve(N, bookings);
+        stout.write(output.join(' ') + '\n');
+})
+
+stdin.resume();
