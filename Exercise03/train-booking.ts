a7620c6469798c5b508e793f3343d8aa28177f6d
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-22 20:34:38 -0300

    feat: create logic and iterator

--- Exercise03/train-booking.ts
+++ Exercise03/train-booking.ts
@@ -1,3 +1,4 @@
+//@ts-ignore
 const { stdin, stout, exit} = process 
 stdin.resume();
 stdin.setEncoding('utf-8');
@@ -8,3 +9,20 @@ stdin.on('data', function (data) {
     rawInput += data.toString().trim();
     rawInput += '\n';
 })
+
+function solve(N: number, bookings: [number, number, number][]): number[] {
+    const finalResult = new Array(N).fill(10000);
+    const result: { [key: number]: number} ={};
+
+    for (let i = 1; i<= N; i++) {
+        result[i] = 0;
+    }
+
+    for (let booking of bookings) {
+        for (let i = booking[0]; i <= booking[1]; i++) {
+            result[1] = result[1] + booking[2];
+        }
+    }
+
+    return finalResult.map((value, index) => value - result[index + 1])
+}
